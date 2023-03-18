576c03ffbde7e6a1fa2b32990877af4b9fb816dd
qoade <as8art@gmail.com>
2023-03-18 12:57:31 +0300

    Works

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iso646.h>
+#include "tester.c"
 
 double runge(double x) {
     return 1 / (1 + x * x);
@@ -28,11 +29,10 @@ double* tridiagonal_matrix_algorithm(double* a, double* b, double* c, double* d,
 
 double* get_gamma(const double* D, const double* E, int N) {
     double *h = (double*)malloc(N * sizeof(double));
-    double *a = (double*)malloc(N * sizeof(double));
-    double *b = (double*)malloc(N * sizeof(double));
-    double *c = (double*)malloc(N * sizeof(double));
-    double *d = (double*)malloc(N * sizeof(double));
-
+    double *a = (double*)malloc((N - 2) * sizeof(double));
+    double *b = (double*)malloc((N - 2) * sizeof(double));
+    double *c = (double*)malloc((N - 2) * sizeof(double));
+    double *d = (double*)malloc((N - 2) * sizeof(double));
 
     h[0] = 0;
     for (int i = 1; i < N; i++)
@@ -47,18 +47,17 @@ double* get_gamma(const double* D, const double* E, int N) {
 
     for (int i = 1; i < N - 2; i++)
         c[i - 1] = h[i + 1]/6;
-    c[N - 2] = 0;
+    c[N - 3] = 0;
 
     for (int i = 1; i < N - 1; i++)
-        d[i - 1] = (E[i + 1] + E[i])/h[i + 1] - (E[i] - E[i - 1])/h[i];
-
+        d[i - 1] = (E[i + 1] - E[i])/h[i + 1] - (E[i] - E[i - 1])/h[i];
 
     double* gamma = tridiagonal_matrix_algorithm(a, b, c, d, N - 2);
     double* gamma_extend = (double*)malloc(N * sizeof(double));
     gamma_extend[0] = 0;
     gamma_extend[N - 1] = 0;
     for (int i = 1; i < N - 1; i++)
-        gamma_extend[i] = gamma[i];
+        gamma_extend[i] = gamma[i-1];
 
     free(h);
     free(a);
@@ -73,13 +72,16 @@ double* get_coefs(int i, double* D, double* E, int N) {
     double* gamma = get_gamma(D, E, N);
     double *h = (double*)malloc(N * sizeof(double));
     h[0] = 0;
-    for (int i = 1; i < N; i++)
-        h[i] = D[i] - D[i - 1];
-    double a3 = (gamma[i] - gamma[i-1])/(6*h[i]);
-    double a2 = (gamma[i-1] * D[i] - gamma[i] * D[i - 1])/(2 * h[i]);
-    double a1 = (gamma[i] * D[i - 1] * D[i - 1] - gamma[i - 1] * D[i] * D[i])/(2 * h[i]) + (E[i] - E[i - 1])/h[i] - h[i] * (gamma[i] - gamma[i-1]) / 6;
-    double a0 = (gamma[i-1] * D[i]*D[i]*D[i] - gamma[i] * D[i-1]*D[i-1]*D[i-1])/(6*h[i]) + (E[i-1] * D[i] - E[i] * D[i - 1])/h[i] + h[i]*(gamma[i] * D[i - 1] - gamma[i - 1] * D[i]) / 6;
-    double coefs[4] = {a3, a2, a1, a0};
+    for (int j = 1; j < N; j++)
+        h[j] = D[j] - D[j - 1];
+
+    double* coefs = (double*)malloc(4 * sizeof(double));
+    coefs[3] = (gamma[i] - gamma[i-1])/(6*h[i]);
+    coefs[2] = (gamma[i-1] * D[i] - gamma[i] * D[i - 1])/(2 * h[i]);
+    coefs[1] = (gamma[i] * D[i - 1] * D[i - 1] - gamma[i - 1] * D[i] * D[i])/(2 * h[i]) + (E[i] - E[i - 1])/h[i] - h[i] * (gamma[i] - gamma[i-1]) / 6;
+    coefs[0] = (gamma[i-1] * D[i]*D[i]*D[i] - gamma[i] * D[i-1]*D[i-1]*D[i-1])/(6*h[i]) + (E[i-1] * D[i] - E[i] * D[i - 1])/h[i] + h[i]*(gamma[i] * D[i - 1] - gamma[i - 1] * D[i]) / 6;
+    free(gamma);
+    free(h);
     return coefs;
 }
 
@@ -90,7 +92,7 @@ double P(int i, double x, double* D, double* E, int N) {
 
 double Spline(double x, double* D, double* E, int N) {
     for (int i = 1; i < N + 1; i++) {
-        if ((i == 1 and (D[0] <= x and x <= D[1])) and (D[i - 1] < x and x <= D[i]))
+        if ( (D[i - 1] <= x) and (x <= D[i]))
             return P(i, x, D, E, N);
     }
 }
@@ -109,9 +111,18 @@ int main() {
 
     for (int i = 1; i < N; i++) {
         double* a = get_coefs(i, D, E, N);
-        printf("P_%d(x) = %lfx^3 + %lfx^2 + %lfx + %lf", i, a[3], a[2], a[1], a[0]);
+        printf("P_%d(x) = %.3lfx^3 + %.3lfx^2 + %.3lfx + %.3lf\n", i, a[3], a[2], a[1], a[0]);
     }
 
+    while (1) {
+        printf("Input x_0 within D\n");
+        double x;
+        scanf("%lf", &x);
+        if (!x)
+            break;
+        printf("Runge(%.3lf) = %.3lf\n", x, runge(x));
+        printf("Spline(%.3lf) = %.3lf\n", x, Spline(x, D, E, N));
+    }
 
     return 0;
 }
